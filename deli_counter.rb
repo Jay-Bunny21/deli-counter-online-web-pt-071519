ca9cb88cac877f3e76741f309e982c186e823601
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 02:15:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(array,name)
 end
 
 def now_serving(array)
-  if array.count = 0 
+  if array.count == 0 
     puts "There is nobody wating to be served!"
   else
     puts "Currently serving #{array.shift}"
