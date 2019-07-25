0d831380198bac9fa0dec1492e800175e4dbddab
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 02:25:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,11 +4,7 @@ def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else
-    line_two = "The line is currently:"
-    katz_deli.each_with_index do |name, index|
-      line_two += "#{index + 1}. #{name}"
-  end
- puts line_two
+    
  end
  end
 
