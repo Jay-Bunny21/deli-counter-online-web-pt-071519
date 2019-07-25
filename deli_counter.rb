b79ec79c7366c29572f053227f9750743cc0b789
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 02:36:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,11 +17,4 @@ def take_a_number(array,name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
-def now_serving(katz_deli)
-  
-  if katz_deli.length == 0
-    puts "There is nobody wating to be served!"
-  else
-    puts "Currently serving #{katz_deli.shift}."
-  end
-end
+
