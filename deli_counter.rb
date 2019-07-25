c6281dd0cd3e0f0902588030fecf9340523f4464
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 02:30:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,16 @@
 katz_deli = []
-
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  else
+    counter = 0
+    until counter == katz_deli.count
+      line << " #{counter + 1}. #{katz_deli[counter]}"
+      counter += 1
+    end
+    puts line
+ end
+ end
 
 def take_a_number(array,name)
   array.push(name)
