99756210d62fc35a93626fe86ecc73a49ac07ecf
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 02:46:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,15 @@ katz_deli = []
 
 
 def line(katz_deli)
+   if katz_deli.length >= 1
   array = []
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  else
-   katz_deli.each_with_index(1) do |name, index|
-     array.push("#{index}. #{name}")
+  counter = 1
+ katz_deli.each do |name|
+   array.push("#{counter}. #{name}")
+   counter += 1
  end
- puts "The line is currently: #{array.join("")}"
+  puts "The line is currently: #{array.join("")}"
+ else
+   puts "The line is currently empty."
  end
- end
+end
