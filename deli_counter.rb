b4c60f0df84cbd0fe6b73fed94c92d4a74ca4586
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 02:50:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(x)
    array.push("#{counter}. #{name}")
    counter += 1
  end
-  puts "The line is currently: #{array.join("")}"
+  puts "The line is currently: #{array}"
  else
    puts "The line is currently empty."
  end
