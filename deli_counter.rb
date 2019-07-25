0ccb96039cb21afa64688ba87f01da19b6217b6f
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 02:29:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,4 @@
 katz_deli = []
-
-def line(katz_deli)
-  if katz_deli.count == 0
-    puts "The line is currently empty."
-  else
-    counter = 0
-    until counter == katz_deli.count
-      line << " #{counter + 1}. #{katz_deli[counter]}"
-      counter += 1
-    end
-    puts line
- end
- end
-
 def take_a_number(array,name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
@@ -26,3 +12,15 @@ def now_serving(katz_deli)
     puts "Currently serving #{katz_deli.shift}."
   end
 end
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  else
+    counter = 0
+    until counter == katz_deli.count
+      line << " #{counter + 1}. #{katz_deli[counter]}"
+      counter += 1
+    end
+    puts line
+ end
+ end
