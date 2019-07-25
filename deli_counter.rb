9c4682cd5378a96921a0135286d281f86af3b330
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 02:25:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,12 @@ def line(katz_deli)
   if katz_deli.count == 0
     puts "The line is currently empty."
   else
-    
+    counter = 0
+    until counter == katz_deli.count
+      line << " #{counter+1}. #{katz_deli[counter]}"
+      counter += 1
+    end
+    puts line
  end
  end
 
